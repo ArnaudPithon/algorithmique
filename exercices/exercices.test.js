eeb8d9fe5f16d2ca98ee923e1240d670022d026b
Arnaud <arnaud@icarios.net>
2023-08-20 00:27:48 +0200

    Refactorisation des tests
    Meilleure utilisation des matchers

--- exercices/exercices.test.js
+++ exercices/exercices.test.js
@@ -5,77 +5,72 @@ const { isTriangleRectangle, max, max3, max3v2, min3, min3v2, tariff } = require
 describe('chapitre 1', () => {
 
     describe('isTriangleRectangle (1.4 p21)', () => {
-
         const triangle = {
             nonRectangle:[12, 3, 32],
             rectangle: [8, 10, 6],
         }
-
-        test('Should return a boolean at false', () => {
-            const result = isTriangleRectangle(triangle.nonRectangle);
-
-            expect(typeof result === 'boolean' && !result).toBe(true);
+        test('Should return false', () => {
+            expect( isTriangleRectangle(triangle.nonRectangle) )
+                .toBe(false);
         });
-
-        test('Should return a boolean at true', () => {
-            const result = isTriangleRectangle(triangle.rectangle);
-
-            expect(typeof result === 'boolean' && result).toBe(true);
+        test('Should return true', () => {
+            expect( isTriangleRectangle(triangle.rectangle) )
+                .toBe(true);
         });
     });
 
     describe('max (1.5 p21)', () => {
 
         test('Should return a number', () => {
-            expect(typeof max(3, 9) === 'number').toBe(true);
+            expect(typeof max(3, 9)).toBe('number');
         });
 
         test('Should return maximum', () => {
-            expect(max(-1.5, -3) === -1.5).toBe(true);
+            expect(max(-1.5, -3)).toBe(-1.5);
         });
     });
 
     describe('max3 (1.6 p21)', () => {
 
         test('Should return a number', () => {
-            expect(typeof max3(3.03, 9, -1) === 'number').toBe(true);
+            expect(typeof max3(3.03, 9, -1)).toBe('number');
         });
 
         test('Should return maximum', () => {
-            expect(max3(-1.5, -3, 0.2) === 0.2).toBe(true);
+            expect(max3(-1.5, -3, 0.2)).toBe(0.2);
         });
     });
 
     describe('max3v2 (1.7 p21)', () => {
 
         test('Should return a number', () => {
-            expect(typeof max3v2(3.03, 9, -1) === 'number').toBe(true);
+            expect(typeof max3v2(3.03, 9, -1)).toBe('number');
         });
 
         test('Should return maximum', () => {
-            expect(max3v2(-1.5, -3, 0.2) === 0.2).toBe(true);
+            expect(max3v2(-1.5, -3, 0.2)).toBe(0.2);
         });
     });
 
     describe('min3 (1.8 p22)', () => {
 
         test('Should return a number', () => {
-            expect(typeof min3(3.03, 9, -1) === 'number').toBe(true);
+            expect(typeof min3(3.03, 9, -1)).toBe('number');
         });
 
         test('Should return minimum', () => {
-            expect(min3(-1.5, -3, 0.2) === -3).toBe(true);
+            expect(min3(-1.5, -3, 0.2)).toBe(-3);
         });
     });
 
     describe('min3v2 (1.8 p22)', () => {
 
         test('Should return a number', () => {
-            expect(typeof min3v2(3.03, 9, -1) === 'number').toBe(true);
+            expect(typeof min3v2(3.03, 9, -1)).toBe('number');
         });
 
         test('Should return minimum', () => {
-            expect(min3v2(-1.5, -3, 0.2) === -3).toBe(true);
+            expect(min3v2(-1.5, -3, 0.2)).toBe(-3);
         });
     });
 
